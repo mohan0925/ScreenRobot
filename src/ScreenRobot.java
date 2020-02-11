b73e085025be0f609f056d22735f638b45ea01f5
mohan0925 <dharavath.mohan47@gmail.com>
2020-02-11 06:42:03 +0000

    first commit

--- src/ScreenRobot.java
+++ src/ScreenRobot.java
@@ -1,43 +1,82 @@
-
+/**ScreenRobot class is designed to move the robot from a particular position to origin
+ * Directions are given by using the U,D,L,R words based on these values robot is moved
+ * When the robot reaches the origin then the program execution is stopped. 
+ */
 public class ScreenRobot {
-	
+	//variables for storing the values
+	private int Move_up; 
+	private int Move_down;
+	private int Move_left;
+	private int Move_right;
+	private int x,y; // x,y positions values
+
+	/**Default constructor
+	 * This sets Move_up,Move_down,Move_left,Move_right to 0
+	 */
 	public ScreenRobot() 
 	{
-		int l = 0; 
-		int countUp = 0, countDown = 0; 
-		int countLeft = 0, countRight = 0; 
-
+		this.Move_up = 0;
+		this.Move_down = 0; 
+		this.Move_left = 0; 
+		this.Move_right = 0; 
 	}
-	
-	static void finalPosition(String move) 
-	{ 
+	/**sets x and y values
+	 * 
+	 * @param x this value is used to set the x axis position
+	 * @param y this value is used to set the y axis position
+	 */
+	public void set_position(int x,int y)
+	{
+			this.x=x;
+			this.y=y;
+			this.Move_up=y;
+			this.Move_down=0;
+			this.Move_left = 0; 
+			this.Move_right = x; 
 		
-		int l = move.length(); 
-		int countUp = 0, countDown = 0; 
-		int countLeft = 0, countRight = 0; 
-
-		// traverse the instruction string 
-		// 'move' 
-		for (int i = 0; i < l; i++) { 
-
-			// for each movement increment 
-			// its respective counter 
-			if (move.charAt(i) == 'U') 
-				countUp++; 
-				
-			else if (move.charAt(i) == 'D') 
-				countDown++; 
-				
-			else if (move.charAt(i) == 'L') 
-				countLeft++; 
-				
-			else if (move.charAt(i) == 'R') 
-				countRight++; 
-		} 
-
-		// required final position of robot 
-		System.out.println("Final Position: ("
-			+ (countRight - countLeft)+ ", "
-			+ (countUp - countDown) + ")"); 
+	}
+	/**Parameter is passed to direct the robot from current position 
+	 * It direct the robot based on the direction given
+	 * U,D,L,R are the direction for the robot which is passed through the argument
+	 * Based on the argument the robot takes it's position by incrementing the specific variable assigned
+	 * 
+	 * @param move parameter is a string to direct the robot
+	 */
+	public void set_finalPosition(String move) 
+	{
+		if(move.equalsIgnoreCase("u"))
+		{
+			Move_up++;
+		}
+		else if(move.equalsIgnoreCase("d"))
+		{
+			Move_down++;
+		}
+		else if(move.equalsIgnoreCase("l"))
+		{
+			Move_left++;
+		}
+		else if(move.equalsIgnoreCase("r"))
+		{
+			Move_right++;
+		}	
+	}
+	/**Returns the x position
+	 * 
+	 * @return x it returns the current x position of robot
+	 */
+	public int get_X_position()
+	{
+		x=(Move_right - Move_left);
+		return x;
+	}
+	/**Returns the y position
+	 * 
+	 * @return y it returns the current y position of robot
+	 */
+	public int get_Y_position()
+	{
+		y=(Move_up - Move_down);
+		return y;
 	}
 }
--- src/ScreenRobotTest.java
+++ src/ScreenRobotTest.java
@@ -1,20 +1,60 @@
-
-import java.io.*; 
+// import scanner class
 import java.util.Scanner;
 
 public class ScreenRobotTest {
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);	
-//		String move = "UDDLLRUUUDUURUDDUULLDRRRR"; 
-		System.out.println("Enter moves to direct the Robot	:"); 
-		System.out.println("U-up,D-down,L-left,R-right	Write in sequence order"); 
-		String move =sc.nextLine();
-		ScreenRobot ScreenRobot_obj= new ScreenRobot();
-	
-		ScreenRobot_obj.finalPosition(move); 
+	private static Scanner sc; // Variable for the scanner is initialised
 
+	public static void main(String[] args) 
+	{	
+		int x=0,y=0; // variable to store x,y values
+		String start="yes";
+		sc = new Scanner(System.in);	
+		// creating the instance for ScreenRobot class
+		ScreenRobot Robot_instance_object= new ScreenRobot();
+		System.out.println("Robot home position is origin(0,0)"); 
+		System.out.println(); 
+		// User enters the X and  Y positions
+		System.out.println("Enter the start point of robot	:");
+		System.out.println("Enter x position	:");
+		x=sc.nextInt();
+		System.out.println("Enter y position	:");
+		y=sc.nextInt();
+		System.out.println("Robot is at position ("+x+","+y+")"); 
+		sc.nextLine();	
+		Robot_instance_object.set_position(x, y);
+		System.out.println("Do you want to take robot to origin?(yes or no)");
+		String move =sc.nextLine();	
+		if(move.equals("yes"))
+		{
+			System.out.println("Enter moves to direct the Robot	to origin"); 
+			/*Until the robot reaches the origin, loop repeats itself */
+			while(!start.equals("no"))
+			{			
+				System.out.println("Press U-up,D-down,L-left,R-right");
+				String direction=sc.nextLine();
+				Robot_instance_object.set_finalPosition(direction);
+				x=Robot_instance_object.get_X_position();
+				y=Robot_instance_object.get_Y_position();
+				
+				System.out.println("Final Position  (X,Y) = ("+x+","+y+")");
+				
+				/* When condition is true
+				 * It displays the robot final position
+				 * Internal validations is done here
+				 * */
+				if(x==0 && y==0)
+				{
+					start="no";
+					System.out.println("-------------Robot reached the origin (0,0) ----------------");
+				}
+			}
+		}
+		else if(move.equals("no"))
+		{
+			x=Robot_instance_object.get_X_position();
+			y=Robot_instance_object.get_Y_position();
+			System.out.println("Final Position (X,Y) = ("+x+","+y+")");
+		}
 	}
-
 }
